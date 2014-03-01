1bb31407c41d9082289a7290f16c9b6fb37c49c2
dron601 <andrejy601@gmail.com>
2014-03-01 20:07:47 +0200

    first commit

---
+++ TodoListManager/src/il/ac/huji/todolist/toDoAdapter.java
@@ -0,0 +1,13 @@
+package il.ac.huji.todolist;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class toDoAdapter extends ArrayAdapter{
+
+	public toDoAdapter(Context context, int resource) {
+		super(context, resource);
+		// TODO Auto-generated constructor stub
+	}
+
+}
