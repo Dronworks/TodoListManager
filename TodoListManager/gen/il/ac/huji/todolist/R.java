5517f363a18b74ff8bf9d503a6c2eed8e96933a0
dron601 <andrejy601@gmail.com>
2014-03-04 10:14:02 +0200

    "first commit" (add and delete works)

--- TodoListManager/gen/il/ac/huji/todolist/R.java
+++ TodoListManager/gen/il/ac/huji/todolist/R.java
@@ -21,22 +21,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int dialog=0x7f020000;
+        public static final int border=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int editNewItem=0x7f080000;
+        public static final int lstTodoItems=0x7f080001;
+        public static final int menuItemAdd=0x7f080004;
+        public static final int menuItemDelete=0x7f080003;
+        public static final int toDoChecked=0x7f080002;
     }
     public static final class layout {
         public static final int activity_todo_list_manager=0x7f030000;
+        public static final int todoline_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int todo_list_manager=0x7f070000;
+        public static final int context=0x7f070000;
+        public static final int todo_list_manager_menu=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addMenu=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,22 +1,85 @@
 package il.ac.huji.todolist;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class TodoListManagerActivity extends Activity {
-
+	private ArrayList<String> strings = new ArrayList<String>();
+	private Activity my = this;
+	private ListView list;
+	ToDoAdapter ad;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		strings.add("Andrey");
+		strings.add("Tal");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo_list_manager);
+		list = (ListView) findViewById(R.id.lstTodoItems);
+		ad = new ToDoAdapter(strings, my);
+		list.setAdapter(ad);
+		registerForContextMenu(list);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.todo_list_manager, menu);
+		//Inflate the menu. this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.todo_list_manager_menu, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		//What happens when we click the menu.
+		super.onOptionsItemSelected(item);
+		if(item.getItemId() == R.id.menuItemAdd){
+			final EditText totalCost = (EditText) findViewById(R.id.editNewItem);
+			strings.add(totalCost.getText().toString());
+			ad.notifyDataSetChanged();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+		int id = info.position;
+		if(item.getItemId() == R.id.menuItemDelete){
+			strings.remove(id);
+			ad.notifyDataSetChanged();
+			Toast.makeText(getApplicationContext(), "test " + id, Toast.LENGTH_SHORT).show();
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+//		Log.w(TEXT_SERVICES_MANAGER_SERVICE, new Integer(info.position).toString());
+		int pos = info.position;
+		String title = strings.get(pos);
+		menu.setHeaderTitle(title);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.context, menu);
+	}
+
 }
+ 
